d9501194a6db9b95bd75b79b0f34c8afc11e3bc4
Muhammad Akmal Beliuk <akmalbeliuk@gmail.com>
2022-09-25 14:25:41 +0700

    Make something with swiper
    This is my final training project using vue js v2 and using swiper library

---
+++ src/App.vue
@@ -0,0 +1,374 @@
+<template>
+  <div id="app">
+    <header-componenet />
+
+    <!-- Swiper -->
+    <div class="swiper mainSwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="title">
+            <div class="title_bg zoom-animation"></div>
+            <div class="title-wrap opening-text">
+              <div class="title-content">
+                <h1>Musicians you should listen to</h1>
+                <p>
+                  "Music expresses what cannot be said and what impossible to
+                  keep silent." <br />
+                  Victor Hugo
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="band-wrap">
+            <div class="band-background bg_2 zoom-animation"></div>
+            <div class="band-title">
+              <h1>THE 1975</h1>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="about-wrap">
+            <div class="about_background bg_3 zoom-animation"></div>
+            <div class="about-content about_animation">
+              <p>
+                The 1975 are a British band consisting of four members, (from
+                left to right) Ross MacDonald, Adam Hann, Matty Healy, and
+                George Daniel, from Manchester, England.
+              </p>
+              <p>
+                The members of the band met during high school and started
+                playing together in 2002. They went by an array of different
+                names throughout their career, such as Drive Like I Do and
+                Bigsleep, until settling on 'The 1975'.
+              </p>
+              <p>
+                The name, The 1975, was inspired by a Jack Kerouac poetry book
+                that had "1 June, The 1975" written on the back. During the
+                early days of the band, they would perform punk covers in local
+                clubs until they started to write music themselves
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="song_wrap">
+            <div class="song-content">
+              <div class="left-song">
+                <h1>Song Recommendation</h1>
+                <ul class="song-list">
+                  <li>Somebody Else</li>
+                  <li>I Always Wanna Die (Sometimes)</li>
+                  <li>Robbers</li>
+                  <li>Love It If We Made It</li>
+                  <li>Happiness</li>
+                  <li>Chotolate</li>
+                  <li>Girls</li>
+                </ul>
+              </div>
+              <div class="right-song"></div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="band-wrap">
+            <div class="band-background bg_4 zoom-animation"></div>
+            <div class="band-title">
+              <h1>MF DOOM</h1>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="about-wrap">
+            <div class="about_background bg_5 zoom-animation"></div>
+            <div class="about-content about_animation">
+              <p>
+                Daniel Dumile, best known by his stage name MF Doom or simply
+                Doom, was a British-American rapper and record producer. Noted
+                for his intricate wordplay, signature metal mask, and
+                "supervillain" stage persona, Dumile became a major figure of
+                underground hip hop and alternative hip hop in the 2000s.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="section9-wrap">
+            <div class="content-wrap">
+              <div class="left-section9 opening-text"></div>
+              <div class="right-section9">
+                <div class="section9-recomendation">
+                  <h1>Songs Recomendation</h1>
+                  <div class="song-list">
+                    <span>Domsday</span>
+                    <span>Accordion</span>
+                    <span>Raid</span>
+                    <span>Beef Rap</span>
+                    <span>Rhymes Like Dimes</span>
+                    <span>Go with the Flow</span>
+                    <span>One Beer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="band-wrap">
+            <div class="band-background bg_6 zoom-animation"></div>
+            <div class="band-title">
+              <h1>Anderson Paak</h1>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="about-wrap">
+            <div class="about_background bg_7 zoom-animation"></div>
+            <div class="about-content about_animation">
+              <p>
+                Brandon Paak Anderson, better known by his stage name Anderson
+                .Paak, is an American rapper, singer, songwriter, record
+                producer, and drummer. He released his debut mixtape, O.B.E.
+                Vol. 1, in 2012 and went on to release Venice in 2014.In 2016 he
+                followed up with Malibu, which received a nomination for Best
+                Urban Contemporary Album at the Grammy Awards, followed by
+                Oxnard, in 2018
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="anderson-recomendation">
+            <div class="anderson-content">
+              <div class="anderson-left"></div>
+              <div class="anderson-right">
+                <h1>Songs Recomendation</h1>
+                <ul class="song-list">
+                  <li>Might Be</li>
+                  <li>Put Me Through</li>
+                  <li>King James</li>
+                  <li>Headlow</li>
+                  <li>Come Home</li>
+                  <li>Bubblin</li>
+                  <li>All Bad</li>
+                  <li>Make It Better</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="band-wrap">
+            <div class="band-background bg_8 zoom-animation"></div>
+            <div class="band-title">
+              <h1>Hindia</h1>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="about-wrap">
+            <div class="about_background bg_9 zoom-animation"></div>
+            <div class="about-content about_animation">
+              <p>
+                Indonesian musician, Baskara Putra, was originally known as the
+                vocalist of a rock band from Jakarta called Feast. Since 2018,
+                he started his solo career using the name Hindia, when releasing
+                the song No One Will Find Me.
+              </p>
+              <p>
+                Initially, Baskara used the name to distinguish between his real
+                self in real life and on stage.
+              </p>
+              <p>
+                But now, most people know him better by the name Indies than his
+                real name. Not infrequently, when he met people in public, he
+                was called 'Mas Hindia'.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="baskara-recomendation">
+            <div class="baskara-content">
+              <h1>
+                Hindia <br />
+                Songs Recomendation
+              </h1>
+              <hr />
+              <p>
+                Before actually working on the album Menari Dalam Shadows
+                (2019), Baskara Putra diligently released singles which were
+                released from 2018 to 2019.
+              </p>
+              <hr />
+              <ul class="baskara-songs">
+                <li>1. Evakuasi</li>
+                <li>2. Wejangan Mama</li>
+                <li>3. Besok Mungkin Kita Sampai</li>
+                <li>4. Jam Makan Siang ft Matter Mos</li>
+                <li>5. Untuk Apa? / Untuk Apa?</li>
+                <li>6. Secukupnya</li>
+                <li>7. Belum Tidur ft Sal Priadi</li>
+                <li>8. Apapun Yang Terjadi</li>
+                <li>9. Membasuh ft Rara Sekar</li>
+                <li>10. Rumah Ke Rumah</li>
+                <li>11. Mata Air ft Natasha Udu</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="band-wrap">
+            <div class="band-background bg_10 zoom-animation"></div>
+            <div class="band-title">
+              <h1>Seringai</h1>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="about-wrap">
+            <div class="about_background bg_11 zoom-animation"></div>
+            <div class="about-content about_animation">
+              <p>
+                The presence of Seringai as an indie rock band seemed to enliven
+                the world of Indonesian rock music. This Jakarta-based band is
+                part of the Puppen band.
+              </p>
+              <p>
+                The grin, who was born in 2001, was conceived by Arian Arifin or
+                commonly known as Arian13. Arian13 is a former vocalist for the
+                band Puppen. In Seringai, Arian13 also acts as a vocalist.
+                Besides Arian13, there are Ricky Siahaan (guitarist), Sammy
+                (bassist), and Edy Khemod (drummer).
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="seringai-wrap">
+            <div class="seringai-content">
+              <h1>SERIGALA<br />MILITIA</h1>
+              <ul class="seringai-songs">
+                <li>Program Party Seringai</li>
+                <li>Selamanya</li>
+                <li>Kilometer Terakhir</li>
+                <li>Berhenti di 15</li>
+                <li>Dilarang Di Bandung</li>
+                <li>Adrenalin Merusuh</li>
+                <li>Omong Kosong</li>
+                <li>Taring</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="closing-wrap">
+            <div class="opening">
+              <div class="opening-content">
+                <span>T</span>
+                <span>H</span>
+                <span>A</span>
+                <span>N</span>
+                <span>K</span>
+                <span>Y</span>
+                <span>O</span>
+                <span>U</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComponenet from "./components/HeaderComponenet.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponenet,
+  },
+  data() {
+    return {
+      mainSwiper: null,
+    };
+  },
+  methods: {
+    createSwiper() {
+      /* eslint-disable */
+      this.mainSwiper = new Swiper(".mainSwiper", {
+        direction: "vertical",
+        slidesPerView: 1,
+        spaceBetween: 0,
+        mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    },
+  },
+  mounted() {
+    this.createSwiper();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+
+#app {
+  height: 100vh;
+}
+html,
+body {
+  position: relative;
+  height: 100%;
+}
+
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+
+.swiper,
+.swiper-wrapper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
---
+++ src/components/HeaderComponenet.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header">
+    <div class="max-width">
+      <div class="header-content">
+        <a class="logo">A</a>
+        <ul class="header-sosmed">
+          <li class="instagram">
+            <a href="https://www.instagram.com/akmal_beliuk" target="blank"
+              ><i class="fa-brands fa-instagram"></i
+            ></a>
+          </li>
+          <li class="twitter">
+            <a href="https://www.twitter.com/" target="blank"
+              ><i class="fa-brands fa-twitter"></i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponenet",
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  width: 100%;
+  z-index: 3;
+  .max-width {
+    padding: 0 1rem;
+  }
+  .header-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .logo {
+      padding: 10px 15px;
+      background-color: #fa7070;
+      color: #fff;
+      border-radius: 10px;
+      font-weight: 700;
+      font-size: 18px;
+    }
+    .header-sosmed {
+      display: flex;
+      li {
+        list-style: none;
+      }
+      .fa-brands {
+        font-size: 18px;
+        color: #fff;
+        padding: 10px;
+        border-radius: 10px;
+        background: #fa7070;
+        margin-left: 10px;
+      }
+    }
+  }
+}
+</style>
